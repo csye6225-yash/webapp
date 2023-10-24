49a0f1a9a1af681d06d7e5c95b41ded9cb4b6063
Yash Bhatia <113397515+bhatiayash@users.noreply.github.com>
2023-10-24 19:56:59 -0400

    config

--- config/config.js
+++ config/config.js
@@ -5,7 +5,7 @@ module.exports = {
     "username": process.env.MYSQL_USER,
     "password": process.env.MYSQL_PASSWORD,
     "database": process.env.MYSQL_DATABASE,
-    "host": "127.0.0.1",
+    "host": process.env.MYSQL_HOSTNAME,
     "dialect": "mysql"
     
   }
